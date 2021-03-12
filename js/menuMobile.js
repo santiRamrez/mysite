da73b587af0d374219774e91023e3ad0a98204cf
Santiago Ramirez <santi_coding@outlook.com>
2021-03-11 22:06:19 -0300

    Footer added

--- js/menuMobile.js
+++ js/menuMobile.js
@@ -7,3 +7,9 @@ let closeMenu = () => (myNav.style.width = "0%");
 
 bottom.addEventListener("click", showMenu, false);
 closeMenuMobile.addEventListener("click", closeMenu, false);
+
+/*Date at the button of the index page */
+
+const dateObj = new Date();
+const ele = document.getElementById("year");
+ele.textContent = dateObj.getFullYear();
